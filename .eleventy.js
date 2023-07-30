dc4ec664c681b254b8543bf52fcddd7b8ec5b4a7
Jalal Abdul Aziz <jalalabdulaziz@gmail.com>
2023-07-30 20:19:08 +0800

    Fix SEO

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 const fg = require('fast-glob');
 const projectImages = fg.sync(['**/img/**/*', '!**/_site/img/**/*']);
+const { DateTime } = require("luxon");
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
   eleventyConfig.addCollection('gallery', function (collection) {
     return projectImages;
   });
